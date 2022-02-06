8e9f9d76927ad273540ce8fa29e5974b2c766b23
rnoh96 <60715552+rnoh96@users.noreply.github.com>
2022-02-06 03:40:09 -0800

    .

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,19 @@
 $(document).scroll(function() {
     if($(window).scrollTop() > 50){
-
-     $(".navigation").css("background-color","white");
-     $(".navigation > *").css("color", "sienna");
-     $(".main-nav > a:link, .main-nav > a:visited").css("color", "sienna");
-     
-
+        $(".navigation").css("background-color","white");
+        $(".navigation > *").css("color", "sienna");
+        $(".main-nav > a:link, .main-nav > a:visited").css("color", "sienna");
     }else if($(window).scrollTop() < 50){
-
         $(".navigation").css("background-color","transparent");
         $(".navigation > *").css("color", "rgba(255, 255, 255, .7)");
         $(".main-nav > a:link, .main-nav > a:visited").css("color", "rgba(255, 255, 255, .7)");
-
     }
 });
+
+// $(document).ready(function() {
+//     let $skills = $(".skills-set > img")
+
+//     $skills.hover(function() {
+//         $(this).css("top", "5px")
+//     })
+// })
